88fc91b236951c1ce26766af1b053960f4dbc2db
John Sylvester <john@sly.dev>
2022-10-31 13:54:55 +0000

    Add function to fetch all artilces

--- src/Views/AllArticles.js
+++ src/Views/AllArticles.js
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function AllArticles() {
 
 	const [articles, setArticles] = useState([]);
+
+	function fetchAllArticles() {
+		axios.get("https://sly-be-nc-news.herokuapp.com/api/articles")
+			.then(response => {
+				setArticles(response.data.articles);
+			});
+	}
+
+	useEffect(fetchAllArticles, []);
 
 	return <>
 		<h1>All Articles</h1>
