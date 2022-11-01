775bd440e54176f1b78432658cb8ee365d94b733
John Sylvester <john@sly.dev>
2022-11-01 13:50:01 +0000

    Link to single page from ArticleCard component

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function ArticleCard(props) {
 	const { article } = props;
 	return <article>
 		<p>{article.author}</p>
 		<time datetime="{article.created_at}">{article.created_at}</time>
-		<h2>{article.title}</h2>
+		<Link to={`/articles/article/${article.article_id}`}><h2>{article.title}</h2></Link>
 		<footer>
 			<p className="article-topic-link">{article.topic}</p>
 			<p>Comments: {article.comment_count} 💬</p>
