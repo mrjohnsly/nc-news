c4530a70ba2195497c5ae88135580312fd20b98e
John Sylvester <john@sly.dev>
2022-11-03 14:22:08 +0000

    Add function to post new comment to an article

--- src/Components/CommentForm.js
+++ src/Components/CommentForm.js
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { useState } from "react";
 
-export function CommentForm() {
+export function CommentForm(props) {
+
+	const { articleId, setComments } = props;
 
 	const [usernameField, setUsernameField] = useState("");
 	const [commentField, setCommentField] = useState("");
@@ -13,7 +16,36 @@ export function CommentForm() {
 		setCommentField(event.target.value);
 	}
 
-	return <form>
+	function handleCommentSubmit(event) {
+		event.preventDefault();
+
+		setComments((currentComments) => {
+			const newComments = [...currentComments];
+			const newComment = {
+				comment_id: 1,
+				body: commentField,
+				article_id: articleId,
+				author: usernameField,
+				votes: 0
+			};
+			newComments.push(newComment);
+			return newComments;
+		});
+
+		axios.post(`https://sly-be-nc-news.herokuapp.com/api/articless/${articleId}/comments`, {
+			"username": usernameField,
+			"body": commentField
+		})
+			.catch(() => {
+				setComments((currentComments) => {
+					const restoredComments = [...currentComments];
+					restoredComments.pop();
+					return restoredComments;
+				});
+			});
+	}
+
+	return <form onSubmit={handleCommentSubmit}>
 		<label>Username:</label>
 		<br></br>
 		<input type="text" placeholder="Your username" value={usernameField} onChange={updateUsernameField}></input>
@@ -21,5 +53,6 @@ export function CommentForm() {
 		<label>Comment:</label>
 		<br></br>
 		<textarea placeholder="I love this article!" value={commentField} onChange={updateCommentField}></textarea>
+		<button type="submit">Submit</button>
 	</form>;
 }
